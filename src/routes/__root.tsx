d2175c736a997faf6d4b8eff0c64699a203ee6a2
Rao Tahir <rao.tahir@web-whiz.co.uk>
2026-05-19 19:09:18 +0500

    chore(monetization): remove alternate ad network scripts in favor of Google AdSense

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -68,12 +68,6 @@ function RootDocument({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
-        {/* Ad Network */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(s){s.dataset.zone='11028295',s.src='https://n6wxm.com/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`,
-          }}
-        />
         <script
           dangerouslySetInnerHTML={{
             __html: `
